17264e4d4bbff5c2aa15c7fad3e1efbf643348e8
Sanjeev Kumar <sanjeevcse2k23@gmail.com>
2024-05-09 22:57:19 +0530

    project setup

---
+++ src/components/category/Category.jsx
@@ -0,0 +1,76 @@
+import { useNavigate } from "react-router";
+
+// category 
+const category = [
+    {
+        image: 'https://cdn-icons-png.flaticon.com/256/12142/12142416.png',
+        name: 'Laptops'
+    },
+    {
+        image: 'https://cdn-icons-png.flaticon.com/128/1753/1753751.png',
+        name: 'Desktops'
+    },
+    {
+        image: 'https://cdn-icons-png.flaticon.com/128/3474/3474362.png',
+        name: 'Monitors'
+    },
+    {
+        image: 'https://cdn-icons-png.flaticon.com/128/4154/4154727.png',
+        name: 'Keyboards'
+    },
+    {
+        image: 'https://cdn-icons-png.flaticon.com/256/12142/12142416.png',
+        name: 'laptop'
+    },
+    {
+        image: 'https://cdn-icons-png.flaticon.com/128/2248/2248732.png',
+        name: 'Mouse'
+    },
+    {
+        image: 'https://cdn-icons-png.flaticon.com/128/2061/2061087.png',
+        name: 'Speakers'
+    },
+    {
+        image: 'https://cdn-icons-png.flaticon.com/128/2729/2729081.png',
+        name: 'Accessories'
+    }
+]
+
+const Category = () => {
+    // naviaget 
+    const navigate = useNavigate();
+    return (
+        <div>
+            <div className="flex flex-col mt-5">
+                {/* main 1 */}
+                <div className="flex overflow-x-scroll lg:justify-center  hide-scroll-bar">
+                    {/* main 2  */}
+                    <div className="flex ">
+                        {/* category  */}
+                        {category.map((item, index) => {
+                            return (
+                                <div key={index} className="px-3 lg:px-10">
+                                    {/* Image  */}
+                                    <div onClick={() => navigate(`/category/${item.name}`)} className=" w-16 h-16 lg:w-24 lg:h-24 max-w-xs rounded-full  bg-pink-500 transition-all hover:bg-pink-400 cursor-pointer mb-1 " >
+                                        <div className="flex justify-center mb-12">
+                                            {/* Image tag  */}
+                                            <img src={item.image} alt="img" />
+                                        </div>
+                                    </div>
+
+                                    {/* Name Text  */}
+                                    <h1 className=' text-sm lg:text-lg text-center font-medium title-font first-letter:uppercase '>{item.name}</h1>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+
+            {/* style  */}
+            <style dangerouslySetInnerHTML={{ __html: "\n.hide-scroll-bar {\n  -ms-overflow-style: none;\n  scrollbar-width: none;\n}\n.hide-scroll-bar::-webkit-scrollbar {\n  display: none;\n}\n" }} />
+        </div>
+    );
+}
+
+export default Category;
---
+++ src/components/testimonial/Testimonial.jsx
@@ -0,0 +1,54 @@
+/* eslint-disable react/no-unescaped-entities */
+
+const Testimonial = () => {
+    return (
+        <div>
+            <section className="text-gray-600 body-font mb-10">
+                {/* main  */}
+                <div className="container px-5 py-10 mx-auto">
+                    {/* Heading  */}
+                    <h1 className=' text-center text-3xl font-bold text-black' >Testimonial</h1>
+                    {/* para  */}
+                    <h2 className=' text-center text-2xl font-semibold mb-10' >What our <span className=' text-pink-500'>customers</span> are saying</h2>
+
+                    <div className="flex flex-wrap -m-4">
+                        {/* Testimonial 1 */}
+                        <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+                            <div className="h-full text-center">
+                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://ecommerce-sk.vercel.app/img/kamal.png" />
+                                <p className="leading-relaxed">Ordered a laptop online and received it within a few days. The packaging was secure, and the product was exactly as described. Will definitely shop here again.</p>
+                                <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
+                                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Kamal Nayan Upadhyay</h2>
+                                <p className="text-gray-500">Samastipur, Bihar</p>
+                            </div>
+                        </div>
+
+                        {/* Testimonial 2 */}
+                        <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+                            <div className="h-full text-center">
+                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://www.devknus.com/img/gawri.png" />
+                                <p className="leading-relaxed">At the computer shop, I found exactly what I was looking for. The staff were incredibly knowledgeable and helpful, making the experience enjoyable. I highly recommend them for their great selection and top-notch service!</p>
+                                <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
+                                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">S Mishra</h2>
+                                <p className="text-gray-500">Samastipur, Bihar</p>
+                            </div>
+                        </div>
+
+                        {/* Testimonial 3 */}
+                        <div className="lg:w-1/3 lg:mb-0 p-4">
+                            <div className="h-full text-center">
+                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://firebasestorage.googleapis.com/v0/b/devknus-official-database.appspot.com/o/images%2FScreenshot%202023-07-07%20at%202.20.32%20PM-modified.png?alt=media&token=324ddd80-2b40-422c-9f1c-1c1fa34943fa" />
+                                <p className="leading-relaxed">Had an issue with a product, and the customer service team went above and beyond to resolve it quickly. Very impressed with their professionalism and dedication to customer satisfaction.</p>
+                                <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
+                                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Sanjay Kumar </h2>
+                                <p className="text-gray-500">Samastipur, Bihar</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Testimonial
